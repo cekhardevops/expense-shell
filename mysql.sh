1ecf22ceafacde73aad5c51bb95c82805341d5f4
cekhardevops <devsecmlops@gmail.com>
2024-10-06 12:30:24 +0530

    01-script

--- mysql.sh
+++ mysql.sh
@@ -63,7 +63,7 @@ log_error() {
 validate_user $user_id
 server
 dnf list installed mysql-server &>>$LOG_FILE
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
     log_info "mysql-server aleady installed"
     exit 1;
 fi
